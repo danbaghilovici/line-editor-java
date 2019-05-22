47c9c584d3c451ad61fb992e698251ce2705c0b7
Dan <dan.baghilovici@gmail.com>
2019-05-22 12:03:16 +0200

    Add editing and state methods to Editor class.

--- src/Editor.java
+++ src/Editor.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class Editor {
+    public static final String DEFAULT_FILENAME="NO NAME";
+
     private List<String> fileLines;
     private ListIterator<String> linesIterator;
     private boolean modified;
@@ -16,7 +18,6 @@ public class Editor {
         // Initialize empty params
         this.fileLines=new ArrayList<String>();
         this.linesIterator=fileLines.listIterator();
-        this.fileName="no_name";
     }
 
     public Editor(String filename) throws IOException {
@@ -25,23 +26,7 @@ public class Editor {
         this.fileLines=new ArrayList<String>();
 
         // Start reading the file
-        BufferedReader bufferedReader = null;
-        FileReader fileReader=null;
-
-        fileReader=new FileReader(filename);
-        bufferedReader=new BufferedReader(fileReader);
-
-        String line;
-        while((line=bufferedReader.readLine())!=null){
-            this.fileLines.add(line);
-        }
-
-        // Close the file pointer
-        if (bufferedReader!=null)
-            bufferedReader.close();
-
-        if (fileReader!=null)
-            fileReader.close();
+        readFile(filename);
 
         // Initialize the list iterator
         // and assign name to the file
@@ -60,19 +45,24 @@ public class Editor {
         this.linesIterator=this.fileLines.listIterator();
     }
 
-    public void loadFile() throws IOException {
+    public void readFile() throws IOException {
         // Clear the contents of the old file
         this.fileLines.clear();
+        // Read file
+        readFile(this.fileName);
+        this.modified=false;
+        this.linesIterator=this.fileLines.listIterator();
 
-        Path pathToFile = Paths.get(this.fileName);
+    }
+
+    private void readFile(String filename) throws IOException {
+        Path pathToFile = Paths.get(filename);
         BufferedReader bufferedReader = Files.newBufferedReader(pathToFile);
         String line ;
         while ((line = bufferedReader.readLine()) != null) {
             this.fileLines.add(line);
         }
-        this.modified=false;
-        this.linesIterator=this.fileLines.listIterator();
-
+        bufferedReader.close();
     }
 
     public String toString(){
@@ -80,8 +70,8 @@ public class Editor {
         StringBuilder result=new StringBuilder();
         ListIterator<String> auxIter=this.fileLines.listIterator();
         if (!auxIter.hasNext()){
-            result.append("-- Empty file -- \n----------------\n");
-            return result.append(lineCounter+">").toString();
+            return result.append("-- Empty file -- \n").toString();
+            //return result.append(lineCounter+">").toString();
         }
         while(auxIter.hasNext()){
             result.append((lineCounter++)+
@@ -89,6 +79,98 @@ public class Editor {
                     auxIter.next()+"\n");
 
         }
+        if (auxIter.nextIndex()==linesIterator.nextIndex()){
+            result.append(lineCounter+">\n");
+        }
         return result.toString();
     }
+
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+        this.modified=true;
+    }
+
+    public void nextLine(){
+        if (this.linesIterator.hasNext()){
+            this.linesIterator.next();
+        }
+    }
+
+    public void previousLine(){
+        if (this.linesIterator.hasPrevious()){
+            this.linesIterator.previous();
+        }
+    }
+
+    public void insertLine(String newLine){
+        this.linesIterator.add(newLine);
+        this.modified=true;
+    }
+
+    public void removeLine(){
+        if (this.linesIterator.hasNext()){
+            this.linesIterator.remove();
+            this.modified=true;
+        }
+
+    }
+
+    public String getCurrentLine(){
+        return this.linesIterator.hasNext()?this.fileLines.get(linesIterator.nextIndex()):null;
+    }
+
+    public void replaceLine(String newLine){
+        if(this.linesIterator.hasNext()){
+            this.linesIterator.set(newLine);
+        }else{
+            this.linesIterator.add(newLine);
+        }
+        this.modified=true;
+    }
+
+    public void goToLine(int lineNumber){
+        ListIterator<String> auxIter;
+        if (lineNumber<=1){
+            this.linesIterator=fileLines.listIterator();
+        }else{
+            if (lineNumber>=this.fileLines.size()){
+                auxIter=this.fileLines.listIterator();
+                while (auxIter.hasNext()){
+                    auxIter.next();
+                }
+            }else{
+                auxIter=this.fileLines.listIterator();
+                while(auxIter.hasNext()){
+                    if (auxIter.nextIndex()==lineNumber){
+                        break;
+                    }
+                }
+            }
+            this.linesIterator=auxIter;
+        }
+    }
+
+    public boolean getState(){
+        //
+        return this.modified;
+    }
+
+    public void clearState(){
+        this.modified=false;
+    }
+
+    public String infoString(){
+        //TO DO
+        return null;
+    }
+
+
+
+
+
+
 }
--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,12 @@ import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        Editor ed=new Editor();
+        Editor ed=new Editor("test.txt");
         System.out.println(ed.toString());
+        System.out.println(ed.getCurrentLine());
+
+
+
 
     }
 }
